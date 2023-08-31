b4fe662fee3118afdd0f2c0963e78fcd801587c5
backmanification <filip.backman@cern.ch>
2023-08-31 13:40:42 +0200

    production mode activated

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def index():
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
 
 """
 from flask import Flask, render_template, request
