3763ab459dff8e7c381e396cb07c67ed1a9c2cf3
backmanification <filip.backman@cern.ch>
2023-09-05 10:46:36 +0200

    functioning version remembering the top search option

--- app.py
+++ app.py
@@ -1,28 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request                                                                                              
 from itertools import combinations
 import scripts
 import requests
 
+from jinja2 import Environment
+from builtins import len
+
 app = Flask(__name__)
+
+app.jinja_env.globals.update(len=len)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     result = {}
+    search_terms = []
+    fieldCount = 1
 
     if request.method == 'POST':
-        fieldCount = len([key for key in request.form if key.startswith('search')])
-        search_terms = []
-        for i in range(1, fieldCount + 1):
-            search_term = request.form.get(f'search{i}', '').strip()
-            if search_term:
-                search_terms.append(search_term)
+        search_terms = [request.form.get(f'search{i}', '').strip() for i in range(1, 11)]
 
-        # Generate pairs of search terms
+        # Remove empty search terms                                                                                                            
+        search_terms = [term for term in search_terms if term]
+
+        # Generate pairs of search terms                                                                                                       
         search_term_pairs = list(combinations(search_terms, 2))
-        
+
         for pair in search_term_pairs:
             search_result = "N/A"
-            url= scripts.make_url(pair)
+            url = scripts.make_url(pair)
             try:
                 response = requests.get(url)
                 if response.status_code == 200:
@@ -31,13 +36,13 @@ def index():
             except requests.RequestException:
                 pass
 
-            # You can also replace the placeholder URL with the actual URL you want to display
+            # You can also replace the placeholder URL with the actual URL you want to display                                                 
             result[f'{pair[0]} + {pair[1]}'] = {
                 'values': search_result,
-                'url': url  # Replace this with the actual URL
+                'url': url  # Replace this with the actual URL                                                                                 
             }
 
-    return render_template('index.html', result=result)
+    return render_template('index.html', result=result, search_terms=search_terms, fieldCount=fieldCount)
 
 if __name__ == '__main__':
     app.run(debug=True)
