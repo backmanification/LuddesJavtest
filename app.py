344cadc9877caad546a2ea39b0c6e2e2273a1c5a
backmanification <filip.backman@cern.ch>
2023-09-13 15:02:03 +0200

    Sortable list, Loading, Remove button, No entry suggestion fixed

--- app.py
+++ app.py
@@ -16,12 +16,6 @@ def search():
     author_options = []
     db = ""
 
-    """
-    Add following functionality:
-    - If only one name, show all their results
-    - Check to see if each name has any hit. If the result is 0, suggest to check spelling
-    """
-    
     # Retrieve values from all input fields dynamically
     search_terms = [request.form.get(f'search{i}', '').strip() for i in range(1, 101)]  # Assume a maximum of 100 search fields
     author_options =  [request.form.get(f'option{i}', '').strip() for i in range(1, 101)]
@@ -31,43 +25,33 @@ def search():
     # Remove empty search terms and options
     t_st = []
     t_ao =[]
+    warning_mess = ''
     for i in range(len(search_terms)):
         if search_terms[i] != '':
+            _,nArticles = scripts.make_url([search_terms[i],''],db)
+            if int(nArticles) == 0:
+                warning_mess += f'<b>No entries found for {search_terms[i]}</b> </br>'
+                continue
             t_st.append(search_terms[i])
             t_ao.append(author_options[i])
     search_terms = t_st
     author_options = t_ao
-
+    warning_mess += '</br>'
     # Generate pairs of search terms
     search_term_pairs = []
-    warning_mess = ''
-    for i in range(len(search_terms)):
-        _,nArticles = scripts.make_url([search_terms[i],''],db)
-        print({search_terms[i]},nArticles)
-        print("test author",nArticles)
-        if int(nArticles) == 0: 
-            warning_mess += f'No entries found for {search_terms[i]} </br>'
-        for j in range(i+1,len(search_terms)):
-            if (t_ao[i]=='def' and t_ao[j]=='def') or (t_ao[i]=='opp' and t_ao[j]=='opp'): continue
-            search_term_pairs.append((t_st[i],t_st[j]))
-    #search_term_pairs = list(combinations(search_terms, 2))
-    if len(t_st)==1: search_term_pairs = [[t_st[0], '']]
 
-    print("AO:",author_options, "\nST:", search_terms,  "\nDB:", db)
+    for i in range(len(search_terms)):
+        for j in range(i+1,len(search_terms)):
+            if (author_options[i]=='def' and author_options[j]=='def') or (author_options[i]=='opp' and author_options[j]=='opp'): continue
+            search_term_pairs.append((search_terms[i],search_terms[j]))
+    #search_term_pairs = list(combinations(search_terms, 2))
+    if len(search_terms)==1: search_term_pairs = [[search_terms[0], '']]
+
+    #print("AO:",author_options, "\nST:", search_terms,  "\nDB:", db)
     
     for pair in search_term_pairs:
         search_result = "N/A"
-        url,nArticles = scripts.make_url(pair, db)
-        """
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                source_code = response.text
-                search_result = scripts.find_nShared(source_code)
-        except requests.RequestException:
-            pass
-        """
-        
+        url,nArticles = scripts.make_url(pair, db)        
         # You can also replace the placeholder URL with the actual URL you want to display
         if pair[1] == '':
             result[f'{pair[0]}'] = {
